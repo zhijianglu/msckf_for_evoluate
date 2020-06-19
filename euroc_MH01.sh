24afed0dba0379037e8d02321b393e8a3d581b5e
zhijianglu <1242004935@qq.com>
2020-06-19 23:30:44 +0800

    first version

---
+++ euroc_MH01.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source devel/setup.bash && roslaunch msckf_vio msckf_vio_euroc_all.launch
+
---
+++ mynteye_online.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source devel/setup.bash && roslaunch msckf_vio mynt_eye01_all.launch
+
